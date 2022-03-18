c250ad23fc25364d3102a94b05936adc97d782e6
baimempatlima <baimempatlima@gmail.com>
2022-03-18 12:49:53 +0700

    add express-mongo

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,29 @@
 const { MongoClient } = require("mongodb");
 // const dotenv = require("dotenv");
 // dotenv.config();
-const url = process.env.MONGO_URI || "mongodb://baim:baim45@localhost:27017?bayu-native?authSource=admin";
-const client = new MongoClient(url);
+// const url = process.env.MONGO_URI || "mongodb://baim:baim45@localhost:27017?bayu-native?authSource=admin";
+// const client = new MongoClient(url);
+
+const env = process.env.NODE_ENV || "development";
+
+const url = () => {
+  switch (env) {
+    case "development":
+      return "mongodb://baim:baim45@localhost:27017?authSource=admin";
+    case "production":
+      return process.env.MONGO_URI;
+  }
+};
+
+const client = new MongoClient(url());
+
+// const url = process.env.MONGO_URI || "mongodb://baim:baim45@localhost:27017?authSource=admin";
+// const client = new MongoClient(url);
 
 (async () => {
   try {
     await client.connect();
-    console.log(`${url}`, "koneksi ke mongodb berhasil");
+    console.log("koneksi ke mongodb berhasil");
   } catch (e) {
     console.log(e);
   }
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,37 @@
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// dotenv.config();
+// require("dotenv");
 
-const database = process.env.MONGO_URI || "mongodb://baim:baim45@localhost:27017/bayu-native?authSource=admin";
+const env = process.env.NODE_ENV || "development";
 
-mongoose.connect(database, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+let db = new mongoose.Connection();
+
+switch (env) {
+  case "development":
+    mongoose.connect("mongodb://baim:baim45@localhost:27017/bayu-native?authSource=admin");
+    db = mongoose.connection;
+    break;
+  case "production":
+    mongoose.connect(process.env.MONGO_URI);
+    db = mongoose.connection;
+    break;
+}
+db.on("error", () => {
+  console.log("connection error");
+});
+db.once("open", () => {
+  console.log("Server mongooseDatabase terhubung");
 });
 
-mongoose.connection.on("connected", () => {
-  console.log(`${database}`, "Server Mongoose Database terhubung");
-});
+// const database = process.env.MONGO_URI || "mongodb://baim:baim45@localhost:27017/bayu-native?authSource=admin";
+
+// mongoose.connect(database, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// });
+
+// mongoose.connection.on("connected", () => {
+//   console.log(`${database}`, "Server Mongoose Database terhubung");
+// });
 
 // const db = mongoose.connection;
 // db.on("error", console.log.bind(`${database}`, "connection error"));
